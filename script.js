15c388ca1bf02e65f24e59cc314d51fb40de4db1
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-06-14 14:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateclock() {
+	const date = new.date();
+	const formatedate = now.toLocalString();
+	document.getElementById("timer").textContent = formatedate;
+	
+}
+updateclock();
+setInterval(updateClock, 1000);
